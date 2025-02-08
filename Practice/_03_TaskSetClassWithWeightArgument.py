501f5dd8d75a7c7655ca7dd997432550e7f4f1e8
avdhut.ssh <avdhut.ssh@gmail.com>
2025-02-08 12:07:44 +0530

    Practice -> TaskSet Class With Weight Argument Tasks with probability to execute than others

--- Practice/_03_TaskSetClassWithWeightArgument.py
+++ Practice/_03_TaskSetClassWithWeightArgument.py
@@ -16,4 +16,4 @@ class MyUser(User):
     tasks = [SearchProduct]
 
 ##User and tasks in different class
-## Tasks with probability to execute than others
+##Tasks with probability to execute than others
