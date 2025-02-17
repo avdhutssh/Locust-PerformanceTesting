1d375069daeebe3d7a692dca87567513548053fe
avdhut.ssh <avdhut.ssh@gmail.com>
2025-02-17 22:52:59 +0530

    Framework Creation -> UtilHelper -> get_current_time_stamp

--- CommonLib/UtilHelper.py
+++ CommonLib/UtilHelper.py
@@ -4,8 +4,14 @@ import datetime
 
 
 class UtilHelper:
+
     @staticmethod
     def get_random_string(length):
         letters = string.ascii_lowercase
         result_str = ''.join(random.choice(letters) for i in range(length))
         return result_str
+
+    @staticmethod
+    def get_current_time_stamp():
+        now = datetime.now()
+        return datetime.timestamp(now)
