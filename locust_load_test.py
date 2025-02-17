2de56a4fe182f94516ea2ad6ebdff9cdf4724c9a
avdhut.ssh <avdhut.ssh@gmail.com>
2025-02-17 19:14:27 +0530

    Framework Creation -> Locust load test file skeleton

---
+++ locust_load_test.py
@@ -0,0 +1,25 @@
+from locust import events
+
+from UserLib.GuestHttpUser import GuestHttpUser
+from UserLib.RegisteredHttpUser import RegisteredHttpUser
+
+
+@events.test_start.add_listener
+def on_test_start(**kwargs):
+    print(kwargs['environment'].host)
+    print("......... Initiating Load Test .........")
+
+
+@events.test_stop.add_listener
+def on_test_stop(**kwargs):
+    print("........ Load Test Completed ........")
+
+
+class UserGroupA(RegisteredHttpUser):
+    weight = 5
+    RegisteredHttpUser.tasks = []
+
+
+class UserGroupB(GuestHttpUser):
+    weight = 1
+    RegisteredHttpUser.tasks = []
